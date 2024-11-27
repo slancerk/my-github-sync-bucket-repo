de77aef0bc6f87fb6bcb40b769dc2de6616fb8e6
Souvik <psouvik260@gmail.com>
2024-11-27 22:21:40 +0530

    updated ec2 and ecs

--- deploy/ec2/main.tf
+++ deploy/ec2/main.tf
@@ -1,6 +1,6 @@
 # Define the AWS provider
 provider "aws" {
-  region = "ap-south-1"  # Change to your desired AWS region
+  region = "us-east-1"  # Change to your desired AWS region
 }
 
 # Create an S3 bucket
